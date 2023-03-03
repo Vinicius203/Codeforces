f626c087f854453d62543642a5ec3861d4b4e9a0
Vinicius203 <viniciusmartinsign@gmail.com>
2023-03-03 01:08:14 -0300

    Codeforces Exercises - Level 800

---
+++ C/281a.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>>
+
+int main()
+{
+    char word[1000];
+
+    scanf("%[^\n]s", word);
+
+    word[0] = toupper(word[0]);
+    printf("%s\n", word);
+
+    return 0;
+}
